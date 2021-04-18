32e343f92760e20f409343edbb048ffa6538681a
YuraIz <7516890@gmail.com>
2021-04-18 17:24:53 +0300

    added lab1

---
+++ YuraIz/lab_1/lab1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("please, enter 4-digit number\n");
+    scanf("%d", &number);
+    if(number >= 10000) {
+        printf("error, this is not a 4-digit number\n");
+    } else {
+        printf("%04d\n", number % 100 * 100 + number / 100);
+    }
+}
