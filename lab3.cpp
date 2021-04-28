340bf63957b166b5cef9c396493d08c0e9b9dab5
Vadim Selivestrau <74891951+Vadim-Selivestrau@users.noreply.github.com>
2021-04-28 12:29:58 +0300

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,36 @@
+﻿//ВАРИАНТ 3.3.2
+#include <iostream>
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+int main()
+{
+    int m=0, n=0;
+    int k = 0, l = 0;
+    printf("input your size array[n][m]\nn-");
+    scanf_s("%d",&n);
+    printf("m-");
+    scanf_s("%d", &m);
+    printf("input your pozition for '1'\ni-");
+    scanf_s("%d", &k);
+    printf("l-");
+    scanf_s("%d", &l);
+    int** array = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+    {
+        *(array+i) = (int*)malloc(m * sizeof(int));
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            array[i][j] = 1 + max(abs(i - k), abs(j - l));
+            printf("%2d", array[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < n; i++)
+    {
+        free(array[i]);
+    }
+    free(array);
+}
