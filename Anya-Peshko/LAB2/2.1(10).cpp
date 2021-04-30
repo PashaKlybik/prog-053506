ed9355900784ecc9ba2bf57b4a3fb8e42766fd34
Hanna_Peshko_03 <peshko.03@list.ru>
2021-04-30 15:06:57 +0300

    Anya PeshkoAnya Peshko Lab 2 Variant 10 & Variant 6

---
+++ Anya-Peshko/LAB2/2.1(10).cpp
@@ -0,0 +1,87 @@
+﻿//Task 2.1 Variant 10
+#include <stdio.h>
+#include < stdlib.h >
+
+struct car {
+	int price;
+	int year;
+	double engineVolume;
+	int mileage;
+};
+
+
+void AddCar(car &myCar)
+{
+	printf("enter the cost of the car: ");
+	scanf_s("%d", &myCar.price);
+
+	printf("enter the year of issue: ");
+	int yearOfIssue;
+	scanf_s("%d", &yearOfIssue);
+	myCar.year = 2021 - yearOfIssue;
+
+	printf("enter engine displacement: ");
+	scanf_s("%lf", &myCar.engineVolume);
+
+	printf("enter mileage: ");
+	scanf_s("%d", &myCar.mileage);
+}
+
+void PrintInfoAboutAuthor() {
+	printf(" author: Hanna Peshko, group 053506 \n version: 1.1 \n");
+}
+
+void CalculateTheCost(car myCar) {
+	int haulCost, customsClearanceCost, allCost;
+
+	haulCost = 0.5 * myCar.mileage;
+
+	if (myCar.year <= 3 || (myCar.year>10 && myCar.year<14) )
+		customsClearanceCost = 0.6* 1000 * myCar.engineVolume;
+	else if (myCar.year>3 && myCar.year<=10) 
+		if(myCar.engineVolume<=2.5)
+			customsClearanceCost = 0.35 * 1000 * myCar.engineVolume;
+		else 
+			customsClearanceCost = 0.6 * 1000 * myCar.engineVolume;
+	else 
+		customsClearanceCost = 2 * 1000 * myCar.engineVolume;
+
+	allCost = customsClearanceCost + haulCost + myCar.price;
+
+	printf("haul cost $: ");
+	printf ("%d\n", haulCost);
+
+	printf("customs clearance cost $: ");
+	printf("%d\n", customsClearanceCost);
+
+	printf("all cost $: ");
+	printf("%d\n", allCost);
+	
+}
+
+void Menu(car myCar) {
+	system("cls");
+	int choice;
+	fflush(stdin);
+	while (true) {
+		printf(" 1.Add car\n 2.Information about author \n 3.Calculate the cost \n 4.Exit \n");
+		scanf_s("%d", &choice);
+		switch (choice)
+		{
+		case 1: system("cls"); AddCar(myCar); system("pause");  break;
+		case 2: system("cls"); PrintInfoAboutAuthor(); system("pause");  break;
+		case 3: system("cls"); CalculateTheCost(myCar); system("pause"); break;
+		case 4: system("cls"); exit(0); break;
+		}
+		system("cls");
+	}
+}
+
+int main(){
+	car myCar;
+	AddCar(myCar);
+	while (true) {
+		Menu(myCar);
+	
+	}
+}
---
+++ Anya-Peshko/LAB2/2.2(6).cpp
@@ -0,0 +1,49 @@
+﻿//Task 2.2 Variant 6
+#include <stdio.h>
+#include <math.h>
+
+const double PI = 3.14;
+
+int fact(int a) {
+    if (a == 0)
+        return 0;
+    if (a == 1)
+        return 1;
+    return  a * fact(a - 1);
+}
+
+int result(int x, double e) {//computation by iteration function
+    int n = 0;
+    double curr = 0;
+    do {
+        n++;
+        curr += pow(-1, n - 1 ) * pow(x, 2 * n - 1)  / fact(2 * n - 1);
+    } while (fabs(sin(x) - curr) >= e);
+    return n;
+}
+
+double sin(int n, double x) { //recursive computation
+    if (n == 1)
+        return x;
+    if (((n - 1) / 2) % 2 != 0)
+        return  sin(n - 2, x) - (pow(x, n) / fact(n));
+    else
+        return  sin(n - 2, x) + (pow(x, n) / fact(n));
+}
+
+int main(){
+    double eps, x;
+    printf("Enter x: ");
+    scanf_s("%lf", &x);
+    printf("Sin x by inline function: ");
+    printf("%f\n", sin(x));
+    printf("Sin x by recursive function: ");
+    printf("%lf\n", sin(5, x));
+    printf("Enter epsilon: ");
+    scanf_s("%lf", &eps);
+    printf("n at which the investigated expression differs from sin x by less than the specified error e:");
+    printf(" = %d \n", result(x, eps));
+
+    return 0;
+}
+
