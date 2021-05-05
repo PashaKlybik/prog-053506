5ee1fa8e7423946264dccc1721f612f52e341488
YuraIz <7516890@gmail.com>
2021-05-05 10:01:02 +0300

    finished lab2t2

--- YuraIz/lab_2/lab2t2.c
+++ YuraIz/lab_2/lab2t2.c
@@ -8,14 +8,14 @@ struct {
         double x;
         int n;
         double value;
-} podCache, dCache;
+} cache;
 
 double POD(double x, int n) {
-        double output = (podCache.x == x && podCache.n == n - 1) ? -podCache.value : (n % 2) ? 1 : -1;
-        for(int i = (podCache.x == x && podCache.n == n - 1) ? 2 * podCache.n : 1; i <= 2 * n - 1; output *= x / i++);
-        podCache.x = x;
-        podCache.n = n;
-        podCache.value = output;
+        double output = (cache.x == x && cache.n == n - 1) ? -cache.value : (n % 2) ? 1 : -1;
+        for(int i = (cache.x == x && cache.n == n - 1) ? 2 * cache.n : 1; i <= 2 * n - 1; output *= x / i++);
+        cache.x = x;
+        cache.n = n;
+        cache.value = output;
 }
 
 double decomposition(double x, int n) {
@@ -25,34 +25,23 @@ double decomposition(double x, int n) {
                         x -= i * PI;
                 }
         }
-        if(dCache.x == x && podCache.n < n) {
-                output = podCache.value;
-                for(int i = podCache.n; i < n; output += POD(x, i++));
-        } else {
-                for(int i = 1; i < n; output += POD(x, i++));
-        }
-        dCache.x = x;
-        dCache.n = n;
-        dCache.value = output;
+        for(int i = 1; i < n; output += POD(x, i++));
 }
 
 int main() {
-        unsigned int n = 1000000000;
-        double e = 0.01;
         double temp = 0;
-        for(int i = 0; i < 10000; i++) {
-                temp = decomposition(5000, i) - sin(5000);
-                if(temp < e && temp > -e) {
-                        printf("%d\n", i);
-                        break;
+        double e = 0;
+        printf("e = ");
+        scanf("%lf", &e);
+        if(e > 1e-20) {
+                for(int n = 1; n < 20; n++) {
+                        temp = decomposition(5, n) - sin(5);
+                        if(temp < e && temp > -e) {
+                                printf("n: %d\n", n);
+                                return 0;
+                        }
                 }
         }
-
-        //printf("%.14lf\n", decomposition(5000, n));
-        //printf("%.14lf\n", sin(5000));
-        //for(double x = 0; x <= 40; x++) {
-        //        printf("x: %d\n", x);
-        //        printf("%.10lf\n", decomposition(x, n));
-        //        printf("%.10lf\n", sin(x));
-        //}
+        printf("Such precision cannot be achieved\n");
+        return 0;
 }
