c99306f5d4cf8f023253d81a44e8f71ae405341f
Максим229 <slysh.2018@mail.ru>
2021-05-15 15:53:27 +0300

    Maksim Slysh lab2 var21 & var21

---
+++ Maksim_Slysh/Lab4/4.1(21).c
@@ -0,0 +1,56 @@
+﻿//task 4.1 var21
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+void ItoB(int n, char s[16], int b)
+{
+    int i = 0,buf=n;
+    for (;n>0;) {
+        buf = n % b;
+        n = n / b;
+        if (buf < 10)
+            s[i] = buf+48;
+        else if (buf == 10)
+            s[i] = 'A';
+        else if (buf == 11)
+            s[i] = 'B';
+        else if (buf == 12)
+            s[i] = 'C';
+        else if (buf == 13)
+            s[i] = 'D';
+        else if (buf == 14)
+            s[i] = 'E';
+        else if (buf == 15)
+            s[i] = 'F';
+        i++;
+    }
+    s[i] = '\0';
+    char bufm;
+    int size = strlen(s);
+    for (int i = 0; i < size/2; i++) {
+        bufm = s[i];
+        s[i] = s[size - 1 - i];
+        s[size - 1 - i] = bufm;
+    }
+    puts(s);
+}
+int main()
+{
+    int n, b, cheack = 0;
+    char s[16];
+    setlocale(LC_ALL, "rus");
+    printf("Введите n: ");
+    scanf_s("%d", &n);
+    printf("Введите b: ");
+    scanf_s("%d", &b);
+    if (b < 2 || b>16)
+        cheack = 1;
+    if (cheack == 0) {
+        ItoB(n, s, b);
+    }
+    else{
+        printf("Не правильный ввод");
+    }
+
+}
+
---
+++ Maksim_Slysh/Lab4/4.2(21).c
@@ -0,0 +1,61 @@
+﻿//task4.2 var21
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char str[255], strtrue[255];
+	FILE* fp = fopen("Test.txt", "r");
+	fgets(str, 254, fp);
+	fclose(fp);
+	int size = strlen(str), count = 0;
+	for (int i = 0; i < size; i++) {
+		strtrue[i] = str[i];
+	}
+	strtrue[size] = '\0';
+	char arraybig[] = { 'Ц','Ю','У','К','Е','Н','Г','Ш','Щ','З','Х','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я','Ч','С','М','И','Т','Б' };
+	char arraysmall[] = { 'ц','ю','у','к','е','н','г','ш','щ','з','х','ф','ы','в','а','п','р','о','л','д','ж','э','я','ч','с','м','и','т','б' };
+	for (int i = 0; i < size - 1; i++) {
+		if (str[i] == '.') {
+			for (int j = 0; j < 29; j++) {
+				if (str[i + 1] == arraysmall[j]) {
+					strtrue[i + 1] = arraybig[j];
+					count++;
+				}
+				else if (str[0] == arraysmall[j]) {
+					strtrue[0] = arraybig[j];
+					count++;
+				}
+			}
+		}
+	}
+	for (int cheack = 0; cheack < 5; cheack++) {
+		for (int i = 0; i < size - 1; i++) {
+			if (strtrue[i] == ' ' && strtrue[i + 1] == ' ') {
+				for (int j = i + 1; j < size - 1; j++) {
+					strtrue[j] = strtrue[j + 1];
+					count++;
+				}
+				size--; strtrue[size] = '\0';
+			}
+		}
+	}
+	int cheack;
+	if (count > 0) {
+		printf("В тексте найдены ошибки:\n");
+		printf("Нажмите 1 Если хотите оставить исходный вариант\n");
+		printf("Нажмите 2 Если хотите исправить\n");
+		scanf_s("%d", &cheack);
+		if (cheack == 2) {
+			FILE* fp = fopen("Test.txt", "w");
+			fprintf(fp, "%s", strtrue);
+		}
+	}
+	else
+		printf("Ошибок не найдено\n");
+	fclose(fp);
+	printf("%s", strtrue);
+}
