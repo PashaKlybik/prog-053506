ccb3f2059f26e6874a869469d8d201e6d073205a
YuraIz <7516890@gmail.com>
2021-05-15 13:24:50 +0300

    modified:   lab4t2.c

--- YuraIz/lab_4/lab4t2.c
+++ YuraIz/lab_4/lab4t2.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Returns vocabulary
 char** scanVocabulary() {
-        int height = 16;
-        char** output = calloc(height, sizeof(char*));
+        const int BUFF_LENGTH = 16;
+        int height = 32;
         FILE* vocabulary = fopen("./lab4t2-data/vocabulary", "r");
-        char* buff = (char*)malloc(32);
-        if(!vocabulary) {
+        if(vocabulary == NULL) {
                 printf("file doesn't exist\n");
                 exit(1);
         }
-        int i;
-        for(i = 0; fgets(buff, 32, vocabulary) != NULL; i++) {
-                for(int j = 0; j < 32; j++) {
+        char** output = (char**)calloc(height, sizeof(char*));
+        char* buff = (char*)malloc(BUFF_LENGTH);
+        for(int i = 0; fgets(buff, BUFF_LENGTH, vocabulary) != NULL; i++) {
+                for(int j = 0; j < BUFF_LENGTH; j++) {
                         if(buff[j] == '\n') {
                                 buff[j] = '\0';
                                 break;
                         }
                 }
                 output[i] = buff;
-                buff = (char*)malloc(32);
+                buff = (char*)malloc(BUFF_LENGTH);
         }
-        output[i] = NULL;
         free(buff);
         fclose(vocabulary);
         return output;
 }
 
+/* Comparing two strings:
+ * if string1 == string2 returns 0
+ * if string1 < string2 returns -1
+ * if string1 > string2 returns 1
+ */
 int compareStrings(char* string1, char* string2) {
         for(int i = 0; string1[i] != '\0' || string2[i] != '\0'; i++) {
                 if(string1[i] < string2[i]) {
@@ -38,34 +43,29 @@ int compareStrings(char* string1, char* string2) {
         return 0;
 }
 
-int find(char** vocabulary, char* string) {
-        for(int i = 0; vocabulary[i] != NULL; i++) {
-                if(compareStrings(string, vocabulary[i]) == 0) {
-                        return i;
+/* Returns index of word in vocabulary
+ * if word doesn't exist returns -1
+ */
+int modernFind(char** vocabulary, char* string) {
+        static int length = 0;
+        if(length == 0) {
+                for(; vocabulary[length] != NULL; length++);
+        }
+        int first = 0, last = length;
+        while(first != last) {
+                int temp = compareStrings(string, vocabulary[(first + last) / 2]);
+                if(temp == 0) {
+                        return (first + last) / 2;
+                } else if(temp == 1) {
+                        first = (first + last) / 2;
+                } else if(temp == -1) {
+                        last = (first + last) / 2;
                 }
         }
         return -1;
 }
 
-char** parse(char* input, char sep) {
-        int length = 0;
-        for(int i = 0; input[i] != '\0'; i++) {
-                if(input[i] == sep) {
-                        length++;
-                }
-        }
-        char** output = (char**)calloc(length + 2, sizeof(char*));
-        output[0] = input;
-        output[length + 1] = NULL;
-        for(int i = 0, j = 0; input[i] != '\0'; i++) {
-                if(input[i] == sep) {
-                        j++;
-                        output[j] = &input[i + 1];
-                }
-        }
-        return output;
-}
-
+// Returns count of uses each words in vocabulary
 void stringFinder(char* text, int* arr, char** vocabulary) {
         char* word;
         for(int i = 0; text[i] != '\0'; i++) {
@@ -81,12 +81,15 @@ void stringFinder(char* text, int* arr, char** vocabulary) {
                                         text[i + j] == '\n' ||
                                         text[i + j] == '\0'
                                         ) {
-                                        word = (char*)malloc(j);
+                                        word = (char*)malloc(j + 1);
                                         for(int n = 0; n < j; n++) {
                                                 word[n] = text[i + n];
                                         }
                                         word[j] = '\0';
-                                        arr[find(vocabulary, word)]++;
+                                        int result = modernFind(vocabulary, word);
+                                        if(result != -1) {
+                                                arr[result]++;
+                                        }
                                         free(word);
                                         break;
                                 }
@@ -102,20 +105,20 @@ int main() {
         int* arr = (int*)calloc(length, sizeof(int));
 
         FILE* textFile = fopen("./lab4t2-data/text", "r");
-        char buff[80];
-        while(fgets(buff, 80, textFile) != NULL) {
+        if(textFile == NULL) {
+                printf("file doesn't exist\n");
+                exit(1);
+        }
+        for(char buff[80]; fgets(buff, 80, textFile) != NULL;) {
                 stringFinder(buff, arr, vocabulary);
         }
+        fclose(textFile);
 
         for(int i = 0; i < length; i++) {
                 printf("%10s: %2d\n", vocabulary[i], arr[i]);
         }
 
-
-        fclose(textFile);
-        free(arr);
-        for(int i = 0; vocabulary[i] != NULL; i++) {
-                free(vocabulary[i]);
-        }
+        for(int i = 0; vocabulary[i] != NULL; free(vocabulary[i++]));
         free(vocabulary);
+        free(arr);
 }
