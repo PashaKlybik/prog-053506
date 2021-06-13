f450ea2395955475ef389ac157dfaa54b5a75a12
YuraIz <7516890@gmail.com>
2021-06-13 17:46:18 +0300

    new file:   4.1.6/lab4t1.cpp new file:   4.2.6/Spisok.txt new file:   4.2.6/lab4t2.cpp

---
+++ Fursanov/4.1.6/lab4t1.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <malloc.h>
+#include <string.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int M, N=41;
+
+	printf("Введите количество строк, которые хотите ввести\nМ = ");
+	while (scanf("%d", &M) <= 0)
+	{
+		printf("Некорректный ввод. попробуйте ещё раз \n");
+		while (getchar() != '\n') {}
+		printf("Введите количество строк, которые хотите ввести\nМ = ");
+	}
+	getchar();
+	printf("Введите %d строк, не превышающие %d символов\n", M, N-1);
+	char**str= (char**)malloc(M * sizeof(char*));
+	for (int i = 0; i < M; i++)
+	{
+			str[i] = (char*)malloc(N * sizeof(char));
+			fgets(str[i], N, stdin);
+			for (int j = 0; j < N; j++)
+				if (str[i][j] == '\n') { str[i][j] = '\0'; break; }
+	}
+	putchar('\n');
+
+	char* p, separator[] = " ,.!?;:";
+	for (int i = 0; i < M; i++)
+	{
+		p = strtok(str[i], separator);
+		while (p)
+		{
+			bool u = 1;
+			for (int i = 0; i + 1 < strlen(p); i++)
+				if (p[i + 1] < p[i])u = 0;
+			if(u)puts(p);
+			p = strtok(0, separator);
+		}
+	}
+
+	return 0;
+}
---
+++ Fursanov/4.2.6/lab4t2.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <malloc.h>
+
+void switch_char(char& a, char& b)
+{
+    char temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    long sWidth = 0, sLen = 40;
+    int a = 1;
+    FILE* myfile;
+    if ((myfile = fopen("Spisok.txt", "r")) == NULL)
+    {
+        printf("Не удалось открыть файл");
+        getchar();
+        return 0;
+    }
+    char ch, ** str = (char**)malloc(sizeof(char*));
+    ch=0; 
+    int count = 0;
+    str[0] = (char*)malloc (sLen * sizeof(char));
+    while (ch != -1)
+    {
+        while ((ch = fgetc(myfile)) != '\n')
+        {
+            if (ch == -1)break;
+            str[sWidth][count] = ch;
+            count++;
+            if(count>=sLen)
+            str[sWidth] = (char*)realloc(str[sWidth],  (sLen*(a++)) * sizeof(char));
+        }
+        str[sWidth][count] = '\0';
+        for (int k = 0; k < sWidth+1; k++)
+        for (int i = 0; i<count; i++)
+        {
+            switch_char(str[sWidth][i], str[sWidth][count - 1]);
+        }
+        str = (char**)realloc(str, (1 + (++sWidth)) * sizeof(char*));
+        str[sWidth] = (char*)malloc(sLen * sizeof(char));
+        count = 0; a = 1;
+    }
+    fclose(myfile);
+    for (int i = 0; i < sWidth; i++)
+        puts(str[i]);
+    return 0;
+}
