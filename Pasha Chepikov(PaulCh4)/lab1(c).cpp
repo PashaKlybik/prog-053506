e372f5f69dfaf0cc88681357b5fe0a7b1970d540
Pavel <79203887+PaulCh4@users.noreply.github.com>
2021-06-16 18:26:14 +0300

    Pasha Chepikov lab1 var28 (без разговоров)

---
+++ Pasha Chepikov(PaulCh4)/lab1(c).cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+
+int Nowlength(char* Arr)
+{
+	int count = 0;
+	for (int i = 0;; i++)
+	{
+		if (Arr[i] == '\0') break;
+		count++;
+	}
+	return count++;
+}
+
+int main(int argc, char* argv[])
+{
+	int N;
+	long a = 1, F = 1, b = 0, count;
+	char Arr[1000] = "1";
+	char str[25] = "";
+
+	printf_s("Enter the number of the digit you want to receive: ");
+	scanf_s("%d", &N);
+	printf_s("\nFibonacci sequence: 1 ");
+
+	count = N;
+
+	while (true)
+	{
+		_itoa_s(F, str, 10);
+		printf_s("%d ", F);
+		strcat_s(Arr, str);
+
+		b = F;
+		F = F + a;
+		a = b;
+
+		if (Nowlength(Arr) >= N) break;
+	}
+	printf_s("...\nNumeral: %c", Arr[N - 1]);
+
+	return(0);
+}
