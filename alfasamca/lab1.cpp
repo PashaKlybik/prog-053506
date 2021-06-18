fd1e0816278ac27c3ee07d76613df5271e3e65b0
YuraIz <7516890@gmail.com>
2021-06-18 21:04:04 +0300

    new file:   alfasamca/lab1.cpp

---
+++ alfasamca/lab1.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+        for (int i = 100; i < 1000; i++)
+    {
+        int b = i, sum = 0;
+        while (b > 9)
+        {
+            int a = b % 10;
+            b = b / 10;
+            sum += pow(a, 3);
+        }
+        sum += pow(b, 3);
+        if (sum == i)
+        {
+            printf("i=%d\n", i);
+        }
+    }
+
+    return 0;
+}
+
+
