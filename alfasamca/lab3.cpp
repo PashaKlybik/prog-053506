96dc536b77dbdbc550ed17e7a4cdafb72a398438
YuraIz <7516890@gmail.com>
2021-06-18 21:02:39 +0300

    new file:   alfasamca/lab3.cpp

---
+++ alfasamca/lab3.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	int arr[a][a];
+	for (int i = 0; i < a; i++)
+	{
+		int q = i + 1;
+		for (int j = 0; j < a; j++)
+		{
+			arr[i][j] = q;
+			q++;
+			if (q > a) q = 1;
+		}
+	}
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < a; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+}
