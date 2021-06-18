2bf9ed12c960b19054d82311b1fe46959b43d9ec
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-06-18 22:37:02 +0300

    Marat Oleg LAB1 var(18)

---
+++ Marat Oleg/LAB_1.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int CountNumberLength(int number) {
+	int length;
+	for (length = 0; ; length++)
+		if ((number /= 10) < 10) break;
+	return length;
+}
+void PrintWithSpaces(int number, int spaces) {
+	for (int i = 0; i < spaces; i++)
+		putchar(' ');
+	printf("%d\n", number);
+}
+int main() {
+	int K = 1500, L = 60, R = K * L;
+	PrintWithSpaces(K, CountNumberLength(R) - CountNumberLength(K));
+	PrintWithSpaces(L, CountNumberLength(R) - CountNumberLength(L));
+	printf("%d\n", R);
+	return 0;
+}
