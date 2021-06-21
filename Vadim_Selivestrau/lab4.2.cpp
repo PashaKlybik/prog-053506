8c205e607983038784052274895abefadcf29f8c
Vadim Selivestrau <74891951+Vadim-Selivestrau@users.noreply.github.com>
2021-06-22 02:07:53 +0300

    Lab 4 (#45)
    * Create variant.txt
    
    * Add files via upload
    
    * Update lab4.2.cpp
    
    * Update lab4.1.cpp
    
    * Update lab4.2.cpp

---
+++ Vadim_Selivestrau/lab4.2.cpp
@@ -0,0 +1,56 @@
+﻿//ВАРИАНТ 4.2.2
+//#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+int main()
+{
+	char* string = (char*)calloc(255, 1);
+	FILE* my_file;
+	my_file = fopen("test.txt", "r");
+	if (my_file == NULL)
+	{
+		printf("error");
+	}
+    while (fgets(string, 50, my_file) != NULL) {}
+    printf("%s", string);
+    fclose(my_file);
+	int vlozhennost = 0;
+	int max_vlozhennost = 0;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		if (string[i] == '(')
+		{
+			string[i] = '{';
+			vlozhennost++;
+			if (vlozhennost > max_vlozhennost)
+			{
+				max_vlozhennost = vlozhennost;
+			}
+		}
+		if (string[i] == ')')
+		{
+			string[i] = '}';
+			vlozhennost--;
+		}
+	}
+	for (int i = 0; i < strlen(string); i++)
+	{
+
+		if (string[i] == '{')
+		{
+			vlozhennost++;
+			if (vlozhennost == max_vlozhennost) string[i] = '(';
+			if (vlozhennost == max_vlozhennost - 1) string[i] = '[';
+		}
+		if (string[i] == '}')
+		{
+			if (vlozhennost == max_vlozhennost) string[i] = ')';
+			if (vlozhennost == max_vlozhennost - 1) string[i] = ']';
+			vlozhennost--;
+		}
+	}
+    printf("\n%s", string);
+    free(string);
+}
