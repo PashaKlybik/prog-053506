1dfa62c5d804caa1c45b0a28562d4eb25d4c30eb
KalininaEkaterina <79200863+KalininaEkaterina@users.noreply.github.com>
2021-06-22 01:47:09 +0300

    Ekaterina Kalinina lab1 var9 (#172)

---
+++ laba1.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(void)
+{
+	system("chcp 1251");
+	system("cls");
+	float chislo;
+	int rub, kop;
+	printf("ведите число : ");
+	scanf_s("%f", &chislo);
+	rub = (int)chislo;
+	kop = (100 * (chislo - rub));
+	printf("%d рублей ", rub);
+	printf("%d копеек", kop);
+	return 0;
+}
