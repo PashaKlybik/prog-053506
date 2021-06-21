ab25d58666e088aad76e4791dbd8b8e41cc47a16
AlfaSamca <79315532+AlfaSamca@users.noreply.github.com>
2021-06-22 02:35:12 +0300

    new file:   alfasamca/lab4t1.cpp (#186)
    new file:   alfasamca/lab4t2.cpp
    
    Co-authored-by: YuraIz <7516890@gmail.com>

---
+++ alfasamca/lab4t1.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+bool palindrome(char* arr);
+int main() {
+    int n;
+    char** massiv;
+
+
+    printf("Number=");
+    scanf("%d", &n);
+
+    massiv = (char**)malloc(n * sizeof(char*));
+
+    for (int i = 0; i < n; i++) {
+        massiv[i] = (char*)malloc(256 * sizeof(char));
+        scanf("%s", massiv[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (palindrome(massiv[i])) printf("%s\n", massiv[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        free(massiv[i]);
+    }
+    free(massiv);
+
+
+
+    return 0;
+}
+bool palindrome(char* arr) {
+    int a = strlen(arr);
+    for (int i = 0; i < a; i++) {
+        if (arr[i] != arr[a - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
