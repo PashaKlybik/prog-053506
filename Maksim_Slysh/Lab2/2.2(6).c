3c09e0985fd0c92ebb910ede6d46cb8ece83f31f
Maksim229 <79194158+Maksim229@users.noreply.github.com>
2021-06-22 02:16:57 +0300

    Maksim Slysh lab2 var8 & var6 (#60)

---
+++ Maksim_Slysh/Lab2/2.2(6).c
@@ -0,0 +1,29 @@
+﻿//lab2.2 var6
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double fact(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return (double)(n * fact(n - 1));
+}
+
+int main()
+{
+	int n=0;
+	double e, x,sum=0;
+	printf("Enter x:");
+	scanf_s("%lf", &x);
+    printf("Enter epsilon:");
+	scanf_s("%lf", &e);
+    do {
+		n++;
+		sum += pow(-1, n - 1) * (pow(x, 2 * n - 1) / fact(2 * n - 1));
+	} while (fabs(sin(x) - sum) >= e);
+    printf("n = %d", n);
+	return 0;
+}
