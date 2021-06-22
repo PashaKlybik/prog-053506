829a20dbe9d1ea74fda93acce598e2e899521602
Elizaveta1412 <79195511+Elizaveta1412@users.noreply.github.com>
2021-06-22 03:32:51 +0300

    ElizavetaBorisenok Laba3 Var 24 (#96)
    * The first lab was created.
    
    * The second lab
    
    * 3d lab
    
    * Develop
    
    * Add files via upload
    
    * Borisyonok Elizaveta Lab_2(25)
    
    * ElizavetaBorisenok Laba3 Var 24

---
+++ Elizaveta Borisyonok/Laba3.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//24 (3.2)
+int main()
+{
+	int m, n;
+	printf("Enter m,n\n");
+	scanf_s("%d", &m);
+	scanf_s("%d", &n);
+
+	int **A = (int **)malloc(n*sizeof(int *));
+    for(int i = 0; i < n; i++)
+    {
+    A[i] = (int *)malloc(m*sizeof(int));
+    }
+
+	int a = -1, b, itog = 1;
+	int max = m * n;
+	int left = -1, right = n - 1, top = 0, bot = m - 1;
+
+	while (left <= right && top <= bot)
+	{
+		for (b = ++left; b <= right && itog <= max; b++)
+		{
+			A[a + 1][b] = itog++;
+		}
+		for (a = ++top; a <= bot && itog <= max; a++)
+		{
+			A[a][b - 1] = itog++;
+		}
+		for (b = --right; b >= left && itog <= max; b--)
+		{
+			A[a - 1][b] = itog++;
+		}
+		for (a = --bot; a >= top && itog <= max; a--)
+		{
+			A[a][b + 1] = itog++;
+		}
+	}
+		for (a = 0; a < m; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				printf("%3d", A[a][b]);
+			}
+			printf("\n");
+		}
+		for(int i = 0; i < n; i++)
+        {
+        free(A[i]);
+        }
+		free(A);
+		return 0;
+}
+
+
